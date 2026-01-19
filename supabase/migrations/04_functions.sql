b4b8d6a6fa96d96c77318f9beca3b2497bf4d0d8
hluengommdd <hluengo@mmddconcepcion.cl>
2026-01-19 00:38:19 -0300

    Create 04_functions.sql with RPC function definitions

---
+++ supabase/migrations/04_functions.sql
@@ -0,0 +1,67 @@
+-- RPC Functions Definition
+
+CREATE FUNCTION stats_kpis() RETURNS TABLE (...) AS $$
+BEGIN
+    -- Function logic here
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION stats_cumplimiento_plazos() RETURNS TABLE (...) AS $$
+BEGIN
+    -- Function logic here
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION stats_reincidencia() RETURNS TABLE (...) AS $$
+BEGIN
+    -- Function logic here
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION stats_mayor_carga() RETURNS TABLE (...) AS $$
+BEGIN
+    -- Function logic here
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION stats_mayor_nivel() RETURNS TABLE (...) AS $$
+BEGIN
+    -- Function logic here
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION stats_promedio_seguimientos_por_caso() RETURNS TABLE (...) AS $$
+BEGIN
+    -- Function logic here
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION stats_tiempo_primer_seguimiento() RETURNS TABLE (...) AS $$
+BEGIN
+    -- Function logic here
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION stats_casos_por_mes() RETURNS TABLE (...) AS $$
+BEGIN
+    -- Function logic here
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION stats_casos_por_tipificacion() RETURNS TABLE (...) AS $$
+BEGIN
+    -- Function logic here
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION stats_casos_por_curso() RETURNS TABLE (...) AS $$
+BEGIN
+    -- Function logic here
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION start_due_process() RETURNS TABLE (...) AS $$
+BEGIN
+    -- Function logic here
+END;
+$$ LANGUAGE plpgsql;
